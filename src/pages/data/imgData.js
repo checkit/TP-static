6f0d426f3401445f49083aaaf2b5bfc76dd5e8e8
checkit <garth.lyerly@gmail.com>
2021-10-16 18:12:35 -0400

    updates based on images

--- src/pages/data/imgData.js
+++ src/pages/data/imgData.js
@@ -1,107 +1,126 @@
-import speedboat from "../../assets/images/slide_img/speedboat-Toddlerpillar.jpg"
-import beeple from "../../assets/images/slide_img/beeple-toddlerpillar.jpg"
-import bonzai from "../../assets/images/slide_img/Bonzai-Toddlerpede (1).jpg"
-import cockroach from "../../assets/images/slide_img/cockroach-head-Toddlerpillar.jpg"
-import dollarmhead from "../../assets/images/slide_img/dollarmhead-Toddlerpillar.jpg"
-import egghead from "../../assets/images/slide_img/egghead-todlerpillars.jpg"
-import grasshead from "../../assets/images/slide_img/grasshead-Toddlerpillar.jpg"
-import f_img from "../../assets/images/slide_img/image0 (1).jpg"
-import s_img from "../../assets/images/slide_img/image0 (2).jpg"
-import z_img from "../../assets/images/slide_img/image0.jpg"
-import jacuzzihead from "../../assets/images/slide_img/jacuzzihead-Toddleerpillars.jpg"
-import jelly from "../../assets/images/slide_img/jelly-Toddlerpillar.jpg"
-import Manny from "../../assets/images/slide_img/Manny-Toddlerpede.jpg"
-import medusa from "../../assets/images/slide_img/medusa-toddlerpede.jpg"
-import monkeynest from "../../assets/images/slide_img/monkeynest-Toddlerpiller.jpg"
-import mushroom from "../../assets/images/slide_img/Mushroom-Toddlerpede.jpg"
-import mushrooms from "../../assets/images/slide_img/mushrooms-toddlerpillar.jpg"
-import pyramid from "../../assets/images/slide_img/Pyramid-Toddlerpede.jpg"
-import snails from "../../assets/images/slide_img/Snails-Toddlerpede.jpg"
-import spaghetti from "../../assets/images/slide_img/Spaghetti-Toddlerpedes.jpg"
+mport speedboat from "../images/slide_img/speedboat-Toddlerpillar.jpg"
+import beeple from "../images/slide_img/beeple-toddlerpillar.jpg"
+import bonzai from "../images/slide_img/Bonzai-Toddlerpillar.jpg"
+import cockroach from "../images/slide_img/cockroach-head-Toddlerpillar.jpg"
+import dollarmhead from "../images/slide_img/dollarmhead-Toddlerpillar.jpg"
+import egghead from "../images/slide_img/egghead-todlerpillars.jpg"
+import grasshead from "../images/slide_img/grasshead-Toddlerpillar.jpg"
+import f_img from "../images/slide_img/image0 (1).jpg"
+import s_img from "../images/slide_img/image0 (2).jpg"
+import z_img from "../images/slide_img/image0.jpg"
+import jacuzzihead from "../images/slide_img/jacuzzihead-Toddlerpillars.jpg"
+import jelly from "../images/slide_img/jelly-Toddlerpillar.jpg"
+import Manny from "../images/slide_img/Manny-Toddlerpillar.jpg"
+import medusa from "../images/slide_img/medusa-toddlerpillar.jpg"
+import monkeynest from "../images/slide_img/monkeynest-Toddlerpillar.jpg"
+import mushroom from "../images/slide_img/Mushroom-Toddlerpillar.jpg"
+import mushrooms from "../images/slide_img/mushrooms-toddlerpillar.jpg"
+import pyramid from "../images/slide_img/Pyramid-Toddlerpede.jpg"
+import snails from "../images/slide_img/Snails-Toddlerpillar.jpg"
+import spaghetti from "../images/slide_img/Spaghetti-Toddlerpillar.jpg"
 
 const ImgData = [
     {
         id: 0,
-        url: speedboat
+        url: speedboat,
+        alt: "speedboat"
     },
     {
         id: 1,
-        url: beeple
+        url: beeple,
+        alt: "beeple"
     },
     {
         id: 2,
-        url: bonzai
+        url: bonzai,
+        alt: "bonzai"
     },
     {
         id: 3,
-        url: cockroach
+        url: cockroach,
+        alt: "cockroach"
     },
     {
         id: 4,
-        url: dollarmhead
+        url: dollarmhead,
+        alt: "dollarmhead"
     },
     {
         id: 5,
-        url: egghead
+        url: egghead,
+        alt: "egghead"
     },
     {
         id: 6,
-        url: grasshead
+        url: grasshead,
+        alt: "grasshead"
     },
     {
         id: 7,
-        url: f_img
+        url: f_img,
+        alt: "unknown"
     },
     {
         id: 8,
-        url: s_img
+        url: s_img,
+        alt: "unknown"
     },
     {
         id: 9,
-        url: z_img
+        url: z_img,
+        alt: "unknown"
     },
     {
         id: 10,
-        url: jacuzzihead
+        url: jacuzzihead,
+        alt: "jacuzzihead"
     },
     {
         id: 11,
-        url: jelly
+        url: jelly,
+        alt: "jelly"
     },
     {
         id: 12,
-        url: Manny
+        url: Manny,
+        alt: "Manny"
     },
     {
         id: 13,
-        url: medusa
+        url: medusa,
+        alt: "medusa"
     },
     {
         id: 14,
-        url: monkeynest
+        url: monkeynest,
+        alt: "monkeynest"
     },
     {
         id: 15,
-        url: mushroom
+        url: mushroom,
+        alt: "mushroom"
     },
     {
         id: 16,
-        url: mushrooms
+        url: mushrooms,
+        alt: "mushrooms"
     },
     {
         id: 17,
-        url: pyramid
+        url: pyramid,
+        alt: "pyramid"
     },
     {
         id: 18,
-        url: snails
+        url: snails,
+        alt: "snails"
     },
     {
         id: 19,
-        url: spaghetti
+        url: spaghetti,
+        alt: "spaghetti"
     }
     
 ]
 
 export default ImgData
-
